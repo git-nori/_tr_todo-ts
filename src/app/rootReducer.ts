6f9afaef5c73c0bd0278293f32fcc49e9c895009
HironoriSato <maza.hs5360@gmail.com>
2020-06-02 08:04:03 +0900

    [add]tasksSliceを実装

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import TasksReducer from "../features/tasks/tasksSlice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  tasks: TasksReducer,
+});
 
 // ステートの型指定に使用
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/features/tasks/tasksSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Task } from "./Types";
+
+type State = {
+  count: number;
+  tasks: Task[];
+};
+
+const initialState: State = {
+  count: 2,
+  tasks: [
+    {
+      id: 2,
+      title: "next task",
+      done: false,
+    },
+    {
+      id: 1,
+      title: "first task",
+      done: true,
+    },
+  ],
+};
+
+const tasksSlice = createSlice({
+  // createSliceを識別するためのname
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask(state: State, action: PayloadAction<string>) {
+      state.count++;
+
+      const newTask: Task = {
+        id: state.count,
+        title: action.payload,
+        done: false,
+      };
+    },
+    doneTask(state: State, action: PayloadAction<Task>) {
+      const task = state.tasks.find((t) => t.id === action.payload.id);
+      if (task) {
+        task.done = !task.done;
+      }
+    },
+    deleteTask(state: State, action: PayloadAction<Task>) {
+      state.tasks = state.tasks.filter((t) => t.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addTask, doneTask, deleteTask } = tasksSlice.actions;
+
+export default tasksSlice.reducer;
